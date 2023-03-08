83c9e69c119916bf65f34078cfc81a961265c87e
ej6 <67990955+ej6@users.noreply.github.com>
2023-03-08 10:56:06 -0500

    Create api.php

---
+++ api.php
@@ -0,0 +1,38 @@
+<?php
+
+if (!isset($_GET['code'])) {
+    die("Error: code parameter not provided");
+}
+
+$code = $_GET['code'];
+
+$url = "https://cfx.re/join/{$code}";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_NOBODY, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion");
+
+$headers = curl_exec($ch);
+
+curl_close($ch);
+
+$headerName = "X-Citizenfx-Url";
+$headerValue = "";
+$headerLines = explode("\r\n", trim($headers));
+foreach ($headerLines as $headerLine) {
+    $headerParts = explode(": ", $headerLine);
+    if (count($headerParts) === 2 && strtolower($headerParts[0]) === strtolower($headerName)) {
+        $headerValue = $headerParts[1];
+        break;
+    }
+}
+
+$ipPort = str_replace(array("http://", "/"), "", $headerValue);
+
+list($ip, $port) = explode(":", $ipPort);
+print("ip={$ip}\nport={$port}");
+
+?>
